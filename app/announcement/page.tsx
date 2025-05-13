1db45e1970f74be741352df7de4e3078d6c2d1f1
Keith <keithwhisper@gmail.com>
2025-05-13 17:05:16 +0900

    feat: add more og

--- app/announcement/page.tsx
+++ app/announcement/page.tsx
@@ -1,8 +1,17 @@
+import type { Metadata } from 'next/types'
 import Hero from './hero'
 import Topics from './topics'
 
 export const dynamic = 'force-static'
 export const revalidate = 3600 // revalidate every hour
+
+export const metadata: Metadata = {
+  title: 'Announcement | Force Bridge Sunset Hub',
+  description: `
+Over the past few years, Force Bridge and Godwoken have played pivotal roles in expanding the Nervos CKB ecosystem — enabling multi-chain asset interoperability, EVM compatibility, and DApp development. These two products marked Nervos' first major steps toward a layered architecture and cross-chain infrastructure.
+
+However, as the industry evolves and the ecosystem pivots toward UTXO-native capabilities, off-chain services, and value-centric automation, it’s time to sunset these early-stage components to make way for the next era of Nervos.`
+}
 
 const Announcement = () => {
   return (
--- app/force-bridge/page.tsx
+++ app/force-bridge/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from 'next/types'
 import { loadHistory } from '@/lib/load-history'
 import Chart from '@/components/chart'
 import tokens from '@/data/tokens.json' assert { type: 'json' }
@@ -6,32 +7,34 @@ import TokenHero from '@/components/token-hero'
 export const dynamic = 'force-static'
 export const revalidate = 3600 // revalidate every hour
 
+export const metadata: Metadata = {
+  title: 'Force Bridge | Force Bridge Sunset Hub',
+  description: `Exit Progress of Force Bridge`
+}
+
 export default function Home() {
   const data = loadHistory()
-  const tokenIds = data[0]?.data.map(i => i.token) ?? []
+  const tokenIds = data[0]?.data.map((i) => i.token) ?? []
 
   return (
     <main className="mx-auto py-20 px-4 flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full max-w-6xl xl:px-0">
       <div className="mx-auto">
-        <TokenHero tokens={tokens.map(t => t.symbol)} platform="Force Bridge" />
+        <TokenHero tokens={tokens.map((t) => t.symbol)} platform="Force Bridge" />
       </div>
-      {tokenIds.map(tokenId => {
-        const token = tokens.find(t => t.args === tokenId)
+      {tokenIds.map((tokenId) => {
+        const token = tokens.find((t) => t.args === tokenId)
         if (!token) return
 
-        const list = data.map(i => {
-          const tokenData = i.data.find(t => t.token === tokenId)
+        const list = data.map((i) => {
+          const tokenData = i.data.find((t) => t.token === tokenId)
           return {
             timestamp: i.timestamp,
-            value: Number(tokenData?.total ?? 0),
+            value: Number(tokenData?.total ?? 0)
           }
         })
 
         return (
-          <div
-            key={tokenId}
-            className="flex flex-col gap-[16px] w-full max-w-6xl"
-          >
+          <div key={tokenId} className="flex flex-col gap-[16px] w-full max-w-6xl">
             <div className="w-full">
               <Chart list={list} token={token} />
             </div>
