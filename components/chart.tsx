9483a57b6aefd5c5815b982217eb288da95049f7
Keith <keithwhisper@gmail.com>
2025-05-15 18:20:21 +0900

    fix: fix explorer link

--- components/chart.tsx
+++ components/chart.tsx
@@ -1,42 +1,38 @@
-"use client"
+'use client'
 /* eslint-disable */
 
 import Link from 'next/link'
-import { CartesianGrid, XAxis, Area, AreaChart, } from "recharts"
+import { CartesianGrid, XAxis, Area, AreaChart } from 'recharts'
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import {
   ChartConfig,
   ChartContainer,
   ChartLegend,
   ChartLegendContent,
   ChartTooltip,
-  ChartTooltipContent,
-} from "@/components/ui/chart"
-import { BigNumber } from "bignumber.js"
-import { ValueType } from "recharts/types/component/DefaultTooltipContent"
+  ChartTooltipContent
+} from '@/components/ui/chart'
+import { BigNumber } from 'bignumber.js'
+import { ValueType } from 'recharts/types/component/DefaultTooltipContent'
 import { formatAmount } from '@/lib/utils'
+import { ckb } from '@/lib/ckb'
+import { SUDT_SCRIPT } from '@/lib/const'
 
-export const description = "An interactive bar chart"
+export const description = 'An interactive bar chart'
 
 const chartConfig = {
   views: {
-    label: "Page Views",
+    label: 'Page Views'
   },
   desktop: {
-    label: "Desktop",
-    color: "hsl(var(--chart-1))",
+    label: 'Desktop',
+    color: 'hsl(var(--chart-1))'
   },
   mobile: {
-    label: "Mobile",
-    color: "hsl(var(--chart-2))",
-  },
+    label: 'Mobile',
+    color: 'hsl(var(--chart-2))'
+  }
 } satisfies ChartConfig
 
 export interface Data {
@@ -44,46 +40,62 @@ export interface Data {
   value: number
 }
 
-
-const CustomChart = ({ list, token }: { list: Array<Data>, token: { logo: string, symbol: string, decimal: number, args: string } }) => {
+const CustomChart = ({
+  list,
+  token
+}: {
+  list: Array<Data>
+  token: { logo: string; symbol: string; decimal: number; args: string }
+}) => {
   const formattedList = list.map((item) => ({
     ...item,
-    value: formatAmount(item.value, token.decimal),
+    value: formatAmount(item.value, token.decimal)
   }))
 
   const total = formattedList[formattedList.length - 1]
+  const tokenId = ckb.utils.scriptToHash({
+    codeHash: SUDT_SCRIPT.CODE_HASH,
+    hashType: SUDT_SCRIPT.HASH_TYPE as CKBComponents.ScriptHashType,
+    args: token.args
+  })
 
   return (
     <Card>
       <CardHeader className="flex flex-col items-stretch space-y-0 border-b p-0 sm:flex-row">
         <div className="flex flex-1 flex-col justify-center gap-1 px-6 py-5 sm:py-6">
           <CardTitle>
-            <img src={token.logo.replace('https://cryptologos.cc', '')} alt={token.symbol} width={24} height={24} className="mr-2 inline-block h-6 w-6" />
-            {token.symbol}</CardTitle>
+            <img
+              src={token.logo.replace('https://cryptologos.cc', '')}
+              alt={token.symbol}
+              width={24}
+              height={24}
+              className="mr-2 inline-block h-6 w-6"
+            />
+            {token.symbol}
+          </CardTitle>
           <CardDescription>
-            <span>Find More information about <b>{token.symbol}</b> on <Link className="underline font-bold" href={`https://explorer.nervos.org/sudt/${token.args}`} target="_blank" rel="noopener noreferrer">CKB Explorer</Link>
-
+            <span>
+              Find More information about <b>{token.symbol}</b> on{' '}
+              <Link
+                className="underline font-bold"
+                href={`https://explorer.nervos.org/sudt/${tokenId}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                CKB Explorer
+              </Link>
             </span>
           </CardDescription>
         </div>
         <div className="flex">
-          <button
-            className="relative z-30 flex flex-1 flex-col justify-center gap-1 border-t px-6 py-4 text-left even:border-l data-[active=true]:bg-muted/50 sm:border-l sm:border-t-0 sm:px-8 sm:py-6"
-          >
-            <span className="text-xs text-muted-foreground">
-              Latest
-            </span>
-            <span className="text-lg font-bold leading-none sm:text-3xl">
-              {BigNumber(total.value).toFormat()}
-            </span>
+          <button className="relative z-30 flex flex-1 flex-col justify-center gap-1 border-t px-6 py-4 text-left even:border-l data-[active=true]:bg-muted/50 sm:border-l sm:border-t-0 sm:px-8 sm:py-6">
+            <span className="text-xs text-muted-foreground">Latest</span>
+            <span className="text-lg font-bold leading-none sm:text-3xl">{BigNumber(total.value).toFormat()}</span>
           </button>
         </div>
       </CardHeader>
       <CardContent className="px-2 sm:p-6">
-        <ChartContainer
-          config={chartConfig}
-          className="aspect-auto h-[250px] w-full"
-        >
+        <ChartContainer config={chartConfig} className="aspect-auto h-[250px] w-full">
           <AreaChart data={formattedList}>
             <defs>
               <linearGradient id="fillValue" x1="0" y1="0" x2="0" y2="1">
@@ -100,9 +112,9 @@ const CustomChart = ({ list, token }: { list: Array<Data>, token: { logo: string
               minTickGap={32}
               tickFormatter={(value) => {
                 const date = new Date(value)
-                return date.toLocaleDateString("en-US", {
-                  month: "short",
-                  day: "numeric",
+                return date.toLocaleDateString('en-US', {
+                  month: 'short',
+                  day: 'numeric'
                 })
               }}
             />
@@ -112,20 +124,21 @@ const CustomChart = ({ list, token }: { list: Array<Data>, token: { logo: string
                 <ChartTooltipContent
                   formatter={(value: ValueType, _, i, __, p: any) => {
                     if (typeof value === 'string') {
-                      return <div className="border-l-2 border-dotted pl-2 flex flex-col">
-                        <div>{new Date(p.timestamp).toLocaleDateString('en-US', {
-                          month: 'short',
-                          day: 'numeric',
-                        })}</div>
-                        <div className="flex gap-4">
-                          <span className="text-muted-foreground">Amount</span>
+                      return (
+                        <div className="border-l-2 border-dotted pl-2 flex flex-col">
+                          <div>
+                            {new Date(p.timestamp).toLocaleDateString('en-US', {
+                              month: 'short',
+                              day: 'numeric'
+                            })}
+                          </div>
+                          <div className="flex gap-4">
+                            <span className="text-muted-foreground">Amount</span>
 
-                          <span>
-                            {BigNumber(value).toFormat()}
-                          </span>
-
+                            <span>{BigNumber(value).toFormat()}</span>
+                          </div>
                         </div>
-                      </div>
+                      )
                     }
                     return value
                   }}
@@ -133,15 +146,8 @@ const CustomChart = ({ list, token }: { list: Array<Data>, token: { logo: string
                 />
               }
             />
-            <Area
-              dataKey="value"
-              type="natural"
-              fill="url(#fillValue)"
-              stroke="var(--color-desktop)"
-              stackId="a"
-            />
+            <Area dataKey="value" type="natural" fill="url(#fillValue)" stroke="var(--color-desktop)" stackId="a" />
             <ChartLegend content={<ChartLegendContent />} />
-
           </AreaChart>
         </ChartContainer>
       </CardContent>
